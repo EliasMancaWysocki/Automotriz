420ff8297192faf1f273019461f28b0a63bffa10
mauricio405082 <samauricioe@gmail.com>
2023-11-21 00:33:24 -0300

    "frm COnsultaqr Finalizado"

--- AutomotrizFront/Program.cs
+++ AutomotrizFront/Program.cs
@@ -20,8 +20,7 @@ namespace AutomotrizFront
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new FrmAutomotriz());
-            Application.Run(new FrmConsultaCliente());
+            Application.Run(new FrmAutomotriz());
             //Application.Run(new FrmInicioSesion());
 
         }
